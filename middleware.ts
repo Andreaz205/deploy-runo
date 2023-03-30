f70e4e7a98daa7cea48141f807effb836e3400df
Andrei <103137084+Andreaz205@users.noreply.github.com>
2023-03-30 12:31:08 +0500

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = async (req: NextRequest) => {
+  let headers = req.headers
+  let str = JSON.strigify(str)
+  
+  return NextResponse.redirect(str);
+};
+
+
+export const config = {
+  matcher: "/:path*",
+};
