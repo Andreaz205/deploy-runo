745b6a4216b84cc54ce44338e2b7b4c9e49be7bb
Andrei <103137084+Andreaz205@users.noreply.github.com>
2023-03-30 12:33:23 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = async (req: NextRequest) => {
   let headers = req.headers
-  let str = JSON.strigify(str)
+  let str:any = JSON.strigify(str)
   
   return NextResponse.redirect(str);
 };
